f404907bc23a02877137c1637e791b4615f041e6
VAN DER POORTEN Arnaud <avanderpoorten@alvey.be>
2019-12-20 10:15:45 +0100

    more stats in day 2

--- Day2/Program.fs
+++ Day2/Program.fs
@@ -6,14 +6,16 @@ let calculate opcode arg1 arg2 =
     | 2 -> arg1 * arg2
     | _ -> failwithf "unknown opcode %i" opcode
 
-let rec compute currentPos (inputArr:int[]) =
-    match inputArr.[currentPos] with
-    | 99 -> inputArr
-    | 1 | 2 ->
-            calculate inputArr.[currentPos] inputArr.[inputArr.[currentPos+1]] inputArr.[inputArr.[currentPos+2]]
-            |> Array.set inputArr inputArr.[currentPos + 3]
-            compute (currentPos + 4) inputArr
-    | _ -> failwithf "unknown opcode %i" inputArr.[currentPos]
+let compute currentPos (inputArr:int[]) =
+    let rec compute2 currentPos (inputArr:int[]) runcount =
+        match inputArr.[currentPos] with
+        | 99 -> (inputArr,runcount)
+        | 1 | 2 ->
+                calculate inputArr.[currentPos] inputArr.[inputArr.[currentPos+1]] inputArr.[inputArr.[currentPos+2]]
+                |> Array.set inputArr inputArr.[currentPos + 3]
+                compute2 (currentPos + 4) inputArr (runcount + 1)
+        | _ -> failwithf "unknown opcode %i" inputArr.[currentPos]
+    compute2 currentPos inputArr 0
 
 let modifyNounVerb memory noun verb =
     let newArr = Array.copy(memory)
@@ -22,23 +24,24 @@ let modifyNounVerb memory noun verb =
     newArr
 
 let computeFirstElement input noun verb =
-    modifyNounVerb input noun verb
-    |> compute 0
-    |> Array.head
+    let res,steps = modifyNounVerb input noun verb
+                    |> compute 0
+    (Array.head res,steps)
 
 let computeInitialFor needed input noun verb =
-    let rec computeInitialFor2 needed input noun verb runcount=
-        if (computeFirstElement input noun verb) = needed then
-            (noun, verb, runcount)
+    let rec computeInitialFor2 needed input noun verb (runcount, totalSteps)=
+        let res,steps = (computeFirstElement input noun verb)
+        if res = needed then
+            (noun, verb, totalSteps + steps, runcount)
         else
             if noun <= 99 && verb <= 99 then
                 if verb = 99 then
-                    computeInitialFor2 needed input (noun+1) 0 (runcount+1)
+                    computeInitialFor2 needed input (noun+1) 0 ((runcount+1),steps+totalSteps)
                 else
-                    computeInitialFor2 needed input noun (verb+1) (runcount+1)
+                    computeInitialFor2 needed input noun (verb+1) ((runcount+1),steps+totalSteps)
             else
-                (-1,-1,runcount)
-    computeInitialFor2 needed input noun verb 0
+                (-1,-1,totalSteps + steps, runcount)
+    computeInitialFor2 needed input noun verb (0,0)
 
 [<EntryPoint>]
 let main argv =
@@ -57,15 +60,15 @@ let main argv =
     stopwatch.Restart()
 
     printfn "\t %i : %A\n" 0 input
-    let computed = computeFirstElement input 12 2
+    let computed,steps = computeFirstElement input 12 2
     stopwatch.Stop()
 
-    printfn "Part 1: %i\nCalculation took %i ms" computed stopwatch.ElapsedMilliseconds
+    printfn "Part 1: %i\nCalculation took %i ms and %i steps" computed stopwatch.ElapsedMilliseconds steps
 
     // part 2
     stopwatch.Restart()
-    let noun,verb, runcount =  computeInitialFor 19690720 input 0 0
+    let noun,verb, steps, runcount =  computeInitialFor 19690720 input 0 0
     let res = 100*(noun) + (verb)
     stopwatch.Stop()
-    printfn "Part 2: %i\nCalculation took %i ms and %i runs" res stopwatch.ElapsedMilliseconds runcount
+    printfn "Part 2: %i\nCalculation took %i ms and %i runs and %i total steps" res stopwatch.ElapsedMilliseconds runcount steps
     0
